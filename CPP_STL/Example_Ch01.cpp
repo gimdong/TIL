112333d5ffc649bb13e58d50830a984ee41658b9
Mac.GDH <gim.dong0312@gmail.com>
2024-09-21 20:52:30 +0900

    - Section02를 위한 새로 branch를 만듬.

--- CPP_STL/Example_Ch01.cpp
+++ CPP_STL/Example_Ch01.cpp
@@ -3,12 +3,17 @@
 int Ch01::Ex01(void)
 {
     int n1 = 10, n2 = 20;
-    cout << n1 + n2 << endl;
+    cout << n1 + n2 << endl; // 기본 연산자
     return 0;
 }
 
+class Point { };
+
 int Ch01::Ex02(void)
 {
-    
+    Point p1, p2;
+
+    //p1 + p2; // Error! 이러한 피연산자와 일치하는 연산자 + 가 없습니다.
+
     return 0;
 }
