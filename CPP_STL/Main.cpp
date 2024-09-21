8d830c384a2d897ebb09be85b7169ddb53d080fe
Mac.GDH <gim.dong0312@gmail.com>
2024-09-21 20:08:39 +0900

    - 파일명을 변경함.

--- CPP_STL/Main.cpp
+++ CPP_STL/Main.cpp
@@ -2,7 +2,10 @@
 
 int main(int argc, char* argv[])
 {
-    cout << "Hello world" << endl;
+    cout << "Hello!!" << endl;
+    cout << "This is gimdong TIL C++ STL Project" << endl;
+
+    cout << "Select example and run!";
 
     Ch01::Ex01();
 
