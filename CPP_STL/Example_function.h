44a7f507147be6661cdb045c22bb5b7daba71958
Mac.GDH <gim.dong0312@gmail.com>
2024-09-22 22:53:01 +0900

    - Section 02 에 해당하는 예제 구현 완료 - Point 클래스의 이항연산자일부를 구현함.

--- CPP_STL/Example_function.h
+++ CPP_STL/Example_function.h
@@ -9,9 +9,26 @@ using namespace std;
 
 // Ch01
 namespace Ch01 {
+
+    class Point
+    {
+    private:
+        int x, y;
+    public:
+        Point(int _x = 0, int _y = 0):x(_x),y(_y) {}
+        void print() const { cout << x << ',' << y << endl;}
+        
+        const Point operator+(Point& arg);
+        const Point operator-(Point& arg);
+        const Point operator*(Point& arg);
+        const Point operator/(Point& arg);
+        const Point operator%(Point& arg);
+    };
+    //Section 01 .. TIL #4
     int Ex01(void);
     int Ex02(void);
-
-    int Ex03(void);
+    //Section 02 .. TIL #5
+    int Ex03_04_05_06(void);
+    int Ex_const_member_function(void);
 }
 
