5bbaa7a125368329bb6a8b16da4030f1b8c8c30b
Mac.GDH <gim.dong0312@gmail.com>
2024-09-22 22:53:01 +0900

    - Section 02 에 해당하는 예제 구현 완료 - Point 클래스의 이항연산자일부를 구현함.

--- CPP_STL/Example_Ch01.cpp
+++ CPP_STL/Example_Ch01.cpp
@@ -1,4 +1,5 @@
 #include "./Example_function.h"
+using namespace Ch01;
 
 int Ch01::Ex01(void)
 {
@@ -6,14 +7,75 @@ int Ch01::Ex01(void)
     cout << n1 + n2 << endl; // 기본 연산자
     return 0;
 }
-
-class Point { };
-
 int Ch01::Ex02(void)
 {
-    Point p1, p2;
-
+    class Temp_Point{};
+    Temp_Point p1, p2;
     //p1 + p2; // Error! 이러한 피연산자와 일치하는 연산자 + 가 없습니다.
+    return 0;
+}
+
+
+const Point Point::operator+(Point& arg)
+{
+    Point pt; 
+    cout << "operator + 호출" << endl; 
+    pt.x = this->x + arg.x;
+    pt.y = this->y + arg.y;
+    return pt;
+}
+const Point Point::operator-(Point& arg)
+{
+    Point pt; 
+    cout << "operator - 호출" << endl; 
+    pt.x = this->x - arg.x;
+    pt.y = this->y - arg.y;
+    return pt;
+}
+const Point Point::operator*(Point& arg)
+{
+    Point pt; 
+    cout << "operator * 호출" << endl; 
+    pt.x = this->x * arg.x;
+    pt.y = this->y * arg.y;
+    return pt;
+}
+const Point Point::operator/(Point& arg)
+{
+    Point pt; 
+    cout << "operator / 호출" << endl; 
+    pt.x = this->x / arg.x;
+    pt.y = this->y / arg.y;
+    return pt;
+}
+const Point Point::operator%(Point& arg)
+{
+    Point pt; 
+    cout << "operator % 호출" << endl; 
+    pt.x = this->x % arg.x;
+    pt.y = this->y % arg.y;
+    return pt;
+}
+
+int Ch01::Ex03_04_05_06(void)
+{
+    Point p1(2,3);
+    Point p2(4,5);
+    Point p3;
+
+    p3 = p1 + p2;
+    p3.print();
+    p3 = p1 - p2;
+    p3.print();
+    p3 = p1 * p2;
+    p3.print();
+    p3 = p1 / p2;
+    p3.print();
+    p3 = p1 % p2;
+    p3.print();
+
+
+
 
     return 0;
 }
--- CPP_STL/Main.cpp
+++ CPP_STL/Main.cpp
@@ -5,11 +5,14 @@ int main(int argc, char* argv[])
     cout << "Hello!!" << endl;
     cout << "This is gimdong TIL C++ STL Project" << endl;
 
-    cout << "Select example and run!";
+    cout << "Select example and run!" << endl;
 
     Ch01::Ex01();
     Ch01::Ex02();
-    Ch01::Ex03();
+
+    Ch01::Ex03_04_05_06();
+   // Ch01::Ex_const_member_function();
+
 
     return 0;
 }
